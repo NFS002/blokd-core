6c97aadc8b816db770b6224b927baf18ed1d61b5
noah <n.fs@hotmail.co.uk>
2023-01-08 23:18:57 +0000

    Small reformating of file

--- src/main/kotlin/blokd/block/actions/Contract.kt
+++ src/main/kotlin/blokd/block/actions/Contract.kt
@@ -1,23 +1,23 @@
 package blokd.block.actions
 
-import com.fasterxml.jackson.annotation.JsonCreator
 import blokd.extensions.hash
-import blokd.extensions.id
 import blokd.extensions.sign
 import blokd.extensions.verifySignature
-import com.fasterxml.jackson.annotation.JsonGetter
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.module.kotlin.KotlinModule
 import java.security.PrivateKey
 import java.security.PublicKey
 import java.time.Instant
-import java.util.UUID
+import java.util.*
 
 
-data class Contract constructor(@JsonProperty("text") val text: String ,@JsonProperty("owner") val owner: PublicKey,@JsonProperty("intendedRecipient") val intendedRecipient: PublicKey) :
+data class Contract constructor(
+    @JsonProperty("text") val text: String,
+    @JsonProperty("owner") val owner: PublicKey,
+    @JsonProperty("intendedRecipient") val intendedRecipient: PublicKey
+) :
     BlockData {
 
-    val createdAt:Long = Instant.now().epochSecond
+    val createdAt: Long = Instant.now().epochSecond
 
     override val encoded = "$text$createdAt$owner$intendedRecipient".hash()
 
@@ -25,7 +25,7 @@ data class Contract constructor(@JsonProperty("text") val text: String ,@JsonPro
 
     override var signature = ByteArray(0)
 
-    override fun sign(privateKey: PrivateKey) : Contract {
+    override fun sign(privateKey: PrivateKey): Contract {
         signature = encoded.sign(privateKey)
         return this
     }
